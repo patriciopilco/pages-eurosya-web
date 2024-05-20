f6a074b4e8489ca392f9af61814aa4cb97f3a411
Patricio Pilco <ppilco@galavail.com>
2024-05-20 16:28:01 -0500

    Actualización del título de la página principal y de la dependencia @supabase/supabase-js a la versión 2.43.2

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl:'https://grpsjakledkqtjnubdhj.supabase.co',
+  supabaseKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdycHNqYWtsZWRrcXRqbnViZGhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMwMzQyNDMsImV4cCI6MjAyODYxMDI0M30.PtgtW24M0NkTS0LV_ioI0DaxoWsDho9xCi4A_k1MT-I',
+};
