8809bc5782f8654970e8a8649ce8b26293576883
Patricio Pilco <ppilco@galavail.com>
2024-06-06 18:34:54 -0500

    add articles

---
+++ src/app/components/summary/components/article/article.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Article } from '../../../../models/article.interface';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.scss'
+})
+export class ArticleComponent {
+  @Input() article!: Article;
+}
