45d4d8766370109d494176fc14bb609eef92f97a
Patricio Pilco <ppilco@galavail.com>
2024-06-07 10:30:59 -0500

    add noticias

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Article } from '../../models/article.interface';
 import { ArticleComponent } from "./components/article/article.component";
 import { CommonModule } from '@angular/common';
+import { SwiperModule } from 'swiper/angular';
+import { SwiperOptions } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
 
 @Component({
     selector: 'app-summary',
     standalone: true,
     templateUrl: './summary.component.html',
     styleUrl: './summary.component.scss',
-    imports: [ArticleComponent, CommonModule]
+    imports: [ArticleComponent, CommonModule,SwiperModule]
 })
 export class SummaryComponent {
 
    public articlesList: Article[] = [];
-
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+    config: SwiperOptions = {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    navigation: false,
+    scrollbar: { draggable: true },
+    loop: true,
+  };
   constructor(private http: HttpClient) {
       this.consumeCloudflareService();
   }
